0672c6e8575c665c4a430dd79e8947fecfe0debc
ashmikan <ashmika.nathali123@gmail.com>
2025-11-19 19:17:01 +0530

    implement tile generation logic

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ MOVE_VEL = 20
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048 Game")
 
-class Title:
+class Tile:
     COLORS = [
         (238, 228, 238),
         (237, 224, 220),
@@ -89,11 +89,31 @@ def draw(window, tiles):
 
     pygame.display.update()
 
+def get_random_pos(tiles):
+    row = None
+    col = None
+    while True:
+        row = random.randrange(0, ROWS)
+        col = random.randrange(0, COLS)
+
+        if f"{row} {col}" not in tiles:
+            break
+
+    return row, col
+
+def generate_tiles():
+    tiles = {}
+    for _ in range(2):
+        row, col = get_random_pos(tiles)
+        tiles[f"{row} {col}"] = Tile(2, row, col)
+
+    return tiles
+
 def main(window):
     clock = pygame.time.Clock()
     run = True
 
-    tiles = {"00": Title(4, 0, 0), "02": Title(128, 2, 0)}  
+    tiles = generate_tiles() 
 
     while run:
         clock.tick(FPS)
